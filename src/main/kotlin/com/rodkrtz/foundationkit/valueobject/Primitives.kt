421171df5b8dd5a7ce0a308a8f3a1ab92f267bd5
rodkrtz <rodkrtz@gmail.com>
2026-03-02 14:19:11 -0300

    ajustes

--- src/main/kotlin/com/rodkrtz/foundationkit/valueobject/Primitives.kt
+++
@@ -1,7 +0,0 @@
-package com.rodkrtz.foundationkit.valueobject
-
-import java.math.RoundingMode
-
-public fun Double.round(decimals: Int): Double {
-    return this.toBigDecimal().setScale(decimals, RoundingMode.CEILING).toDouble()
-}
